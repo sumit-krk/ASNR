7ac785fc64006d4324ae97b2c9971ae9767957c1
sumit-krk <kushakk007@gmail.com>
2024-09-07 21:56:25 +0530

    first commit after long time

---
+++ App.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// const heading=React.createElement("h1",{id:'heding'},"Hello World In React yes");
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading)
--- Inception/App.js
+++
@@ -1,3 +0,0 @@
-const heading=React.createElement("h1",{id:'heding'},"Hello World In React");
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
